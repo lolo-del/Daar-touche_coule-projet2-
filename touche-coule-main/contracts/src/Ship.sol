c75ff8dfc4c6f05bdf5110c170467d87f9cd949f
niisriinee <119232109+niisriinee@users.noreply.github.com>
2022-11-27 19:20:42 +0100

    Update Ship.sol

--- touche-coule-main/contracts/src/Ship.sol
+++ touche-coule-main/contracts/src/Ship.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8;
 
 import 'hardhat/console.sol';
 
-
 abstract contract Ship {
   function update(uint x, uint y) public virtual;
   function fire() public virtual returns (uint, uint);
